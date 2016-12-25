f0311555ea7cce2ccc2486042c7275903689688c
xyakamoz <xuxun@wiz.cn>
2016-12-25 23:54:42 +0800

    Exercise of Python

---
+++ ex17.py
@@ -0,0 +1,31 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+# we could do these two on one line too, how?
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+out_file.close()
+in_file.close()
+
+txt_read_again = open(to_file)
+print txt_read_again.read()
+
+# question
+# 在最后，我想要输出复制对象文件的内容
+# 如果一个对象倍执行了 close() 则无法使用 open() 或 read() 
