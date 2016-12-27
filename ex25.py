ea940d5098152eca8261ec6dceb717b310cbbb81
xyakamoz <xuxun@wiz.cn>
2016-12-27 23:47:18 +0800

    Logic And If-elif-else

--- ex25.py
+++ ex25.py
@@ -29,7 +29,7 @@ def print_first_and_last(sentence):
 	words = break_words(sentence)
 	print_first_word(words)
 	print_last_word(words)
-	
+
 def print_first_and_last_sorted(sentence):
 	"""Sorts the words then prints the first and last one."""
 	words = sort_sentence(sentence)
